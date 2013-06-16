b6dc763b9cffef269f635534b960beee1d8c3fbb
Roberto Moulin <roberto.moulin@corp.globo.com>
2013-06-16 01:07:14 -0300

    Refactor code

--- cadastro_rh/admin.py
+++ cadastro_rh/admin.py
@@ -4,21 +4,14 @@ from mysite.cadastro_rh.models import Cadastro
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.contrib import admin
-# from django.http import HttpResponseRedirect
-# from django.core.mail import send_mail
 
 
 def enviar_email(modeladmin, request, queryset):
     enviar_email.short_description = "Enviar email"
-
     mail_string = ""
     for item in queryset:
-
         mail_string = mail_string + ">>" + str(item.email)
 
-
-    # send_mail('titulo', email_list, 'roberto.rmoulin@gmail.com', email_list)
-    # return HttpResponseRedirect("/login/enviar/")
     return render_to_response('contato.html', {'mail_string': mail_string}, context_instance=RequestContext(request, {}))
 
 
@@ -30,6 +23,3 @@ class ContatoAdmin(admin.ModelAdmin):
     save_on_top = True
 
 admin.site.register(Cadastro, ContatoAdmin)
-
-
-
--- cadastro_rh/views.py
+++ cadastro_rh/views.py
@@ -1,36 +1,38 @@
 # -*- coding:utf-8 -*-
 
 from django.shortcuts import render_to_response
-from django.contrib.auth import authenticate, logout, login as authlogin
 from django.template import RequestContext
 from django.http import HttpResponse
+# from django.contrib.auth import authenticate, logout, login as authlogin
 
 from mysite.cadastro_rh.models import FormContato
-from cadastro_rh.models import Cadastro
+# from cadastro_rh.models import Cadastro
 
 
 def login(request):
-    if request.user.id:
-        return render_to_response('logado.html', {}, context_instance=RequestContext(request, {}))
+    pass
+    # if request.user.id:
+    #     return render_to_response('logado.html', {}, context_instance=RequestContext(request, {}))
 
-    if request.POST:
-        emailUser = request.POST.get('email')
-        senhaUser = request.POST.get('senha')
+    # if request.POST:
+    #     emailUser = request.POST.get('email')
+    #     senhaUser = request.POST.get('senha')
 
-        auth = authenticate(username=emailUser, password=senhaUser)
-        if auth is not None:
-            if auth.is_active:
-                authlogin(request, auth)
-                lista_cadastro = Cadastro.objects.all().order_by('datacadastro')
+    #     auth = authenticate(username=emailUser, password=senhaUser)
+    #     if auth is not None:
+    #         if auth.is_active:
+    #             authlogin(request, auth)
+    #             lista_cadastro = Cadastro.objects.all().order_by('datacadastro')
 
-                return render_to_response('logado.html', {'lista_cadastro': lista_cadastro, 'macaca': 'macaca'}, context_instance=RequestContext(request))
+    #             return render_to_response('logado.html', {'lista_cadastro': lista_cadastro, 'macaca': 'macaca'}, context_instance=RequestContext(request))
 
-    return render_to_response('login.html', {}, context_instance=RequestContext(request, {}))
+    # return render_to_response('login.html', {}, context_instance=RequestContext(request, {}))
 
 
 def sair(request):
-    logout(request)
-    return render_to_response('login.html')
+    pass
+    # logout(request)
+    # return render_to_response('login.html')
 
 
 def enviar_email(request):
