f4cbc0e3c495a95331afcde2dee16c51dd2a7e0e
Roberto Moulin <roberto.moulin@corp.globo.com>
2013-06-16 16:43:05 -0300

    ajuste teste de rota

--- testes/test_views.py
+++ testes/test_views.py
@@ -7,6 +7,6 @@ import os
 class TestViews(unittest.TestCase):
 
     def test_route_should_return_200(self):
-        response = requests.get(os.path.join(URL_SERVICE, 'cadastro'))
+        response = requests.get(os.path.join(URL_SERVICE, 'login/enviar'))
         status = response.status_code
         self.assertEqual(200, status)
